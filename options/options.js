4ac46fe7cad0ad7e473f50814f8949f4fb863831
Crowdbotics <engineering@crowdbotics.com>
2023-06-26 09:25:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-mouse-dev-94807",
+  url: "https://royal-mouse-dev-94807.botics.co",
+  api: "https://royal-mouse-dev-94807.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
